c5a92118e62936628e338c434deb2990b826ba0c
KK44689 <kirima111.04@hotmail.com>
2021-04-16 23:39:34 +0700

    delete additional files

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\PagesController@home');
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
